c0c524261d7d17c3566ccd07217a446122bbd2e8
UNIdavid8 <dmurigar@myuax.com>
2025-02-07 11:27:43 +0100

    pares e impares locos

--- src/Main.java
+++ src/Main.java
@@ -48,5 +48,38 @@ System.out.println("Hello and welcome!\n");
                         default:
                             System.out.print("El color es ERRONEO");
         }
+        int suma1 = 0, suma2 = 0;
+        for(int i = 1; i <= 10; i++){
+            suma1 += i;
+            suma2 = suma2 + i;
+            System.out.printf("SUMA1 = %d + %d = %d\n", i, suma1, suma1+i);
+            System.out.printf("SUMA2 = %d + %d = %d\n", i, suma2, suma2+i);
+        }
+        int numero = 1;
+
+
+        while(numero % 2 != 0 ){
+            System.out.println("INTRODUCE UN NUMERO: ");
+            numero = sc.nextInt();
+
+            if(numero % 2 == 0){
+                System.out.println("El numero " + numero + " es par");
+            }else System.out.println("El numero " + numero + " es impar");
+        }
+        int contador = 0;
+        while(contador != 2){
+            System.out.println("INTRODUCE UN NUMERO: ");
+            numero = sc.nextInt();
+            if(numero % 2 == 0){
+                contador++;
+                System.out.println("El numero " + numero + " es par");
+                System.out.println("contador : "+contador);
+            }
+            if(numero % 2 != 0){
+                contador = 0;
+                System.out.println("El numero " + numero + " es impar");
+                System.out.println("contador : "+contador);
+            }
+        }
     }
 }
